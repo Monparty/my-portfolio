ec6eb7a74e0e1667f76c4971738fc162626b4f7e
mononedigitalnetwork <suniti.suc@onedigitalnetwork.co>
2025-05-31 17:10:27 +0700

    modify responsive

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import cloud from "../../public/images/cloud.png";
 import blockBrick from "../../public/images/blockBrick.png";
 import blockQuestion from "../../public/images/blockQuestion.png";
@@ -22,6 +22,8 @@ import reactLogo from "../../public/images/reactLogo.png";
 import sqlLogo from "../../public/images/sqlLogo.png";
 import tailwindLogo from "../../public/images/tailwindLogo.png";
 import brownMushroom from "../../public/images/brownMushroom.png";
+import arrow from "../../public/images/arrow.png";
+import moncookImg1 from "../../public/images/moncookImg1.webp";
 
 const logoLists = [
 	{
@@ -32,7 +34,7 @@ const logoLists = [
 	{
 		name: "CSS",
 		src: cssLogo,
-		score: 90
+		score: 95
 	},
 	{
 		name: "JavaScript",
@@ -42,12 +44,12 @@ const logoLists = [
 	{
 		name: "ReactJS",
 		src: reactLogo,
-		score: 80
+		score: 60
 	},
 	{
 		name: "NextJS",
 		src: nextLogo,
-		score: 70
+		score: 55
 	},
 	{
 		name: "jQuery",
@@ -77,7 +79,7 @@ const logoLists = [
 	{
 		name: "MySQL",
 		src: sqlLogo,
-		score: 70
+		score: 60
 	},
 	{
 		name: "GitHub",
@@ -92,13 +94,73 @@ const logoLists = [
 ];
 
 export default function Home() {
-	const [isVisible, setIsVisible] = useState(false);
-	const toggleVisibility = () => {
-		setIsVisible(!isVisible);
+	const [progressValue, setProgressValue] = useState(100);
+	const [progressImg, setProgressImg] = useState(htmlLogo);
+	const handleBtnClick = (newValue, newImg) => {
+		if (newValue >= 0 && newValue <= 500) {
+			setProgressValue(newValue);
+			setProgressImg(newImg);
+		} else {
+			setProgressValue(0);
+			setProgressImg(htmlLogo);
+		}
+	}
+
+	const scrollableDivRef = useRef(null);
+
+	const scrollLeft = () => {
+        if (scrollableDivRef.current) {
+            scrollableDivRef.current.scrollBy({
+                left: -200,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRight = () => {
+        if (scrollableDivRef.current) {
+            scrollableDivRef.current.scrollBy({
+                left: 200, 
+                behavior: 'smooth'
+            });
+        }
+    };
+
+	const scrollableDivRefPop = useRef(null);
+
+	const scrollLeftPop = () => {
+        if (scrollableDivRefPop.current) {
+            scrollableDivRefPop.current.scrollBy({
+                left: -160,
+                behavior: 'smooth'
+            });
+        }
+    };
+
+    const scrollRightPop = () => {
+        if (scrollableDivRefPop.current) {
+            scrollableDivRefPop.current.scrollBy({
+                left: 160, 
+                behavior: 'smooth'
+            });
+        }
+    };
+
+	const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+	const openPopup = () => {
+		setIsPopupOpen(true);
+		document.body.style.overflow = 'hidden';
 	};
+
+	const closePopup = () => {
+		setIsPopupOpen(false);
+		document.body.style.overflow = 'auto';
+	};
+
     return (
         <div>
-            <section className="w-full h-dvh bg-[#A2ADFF] relative">
+            <section className="w-full h-dvh bg-[#A2ADFF] relative hidden!">
                 <div className="flex flex-col items-center h-full">
                     <div className="bg-[#DE5819] mt-10 w-fill h-1/3 p-6 rounded-sm border-4 border-t-white border-l-white flex items-center justify-center relative">
                         <div className="flex justify-center items-start flex-col">
@@ -114,7 +176,7 @@ export default function Home() {
                         <div className="dot-shadow w-2 h-2 bg-white absolute bottom-2 left-2"></div>
                         <div className="dot-shadow w-2 h-2 bg-white absolute bottom-2 right-2"></div>
                     </div>
-					<div className="flex flex-col gap-2 absolute top-[20rem]">
+					<div className="flex flex-col gap-2 absolute top-[21rem]">
 						<Link href="#"><span className=" group/item text-white text-xl hover:text-gray-200 flex items-center justify-start gap-2">
 							<Image src={brownMushroom} className="w-8 object-cover invisible group-hover/item:visible" />
 							1 ABOUT</span>
@@ -172,13 +234,13 @@ export default function Home() {
                     </Link>
 					<div className="group/item">
                         <div className="hidden group-hover/item:block">
-                            <section className="message -left absolute left-[18rem] bottom-[13rem]">
+                            <section className="message -left absolute left-[16rem] bottom-[13rem]">
                                 <div class="nes-balloon from-left">
                                     <p>Hi i'm Mon</p>
                                 </div>
                             </section>
                         </div>
-                        <div className="absolute bottom-23 left-[14rem]">
+                        <div className="absolute bottom-23 left-[12rem]">
                             <i className="nes-mario hover:animate-bounce nes-pointer"></i>
                         </div>
                     </div>
@@ -192,7 +254,7 @@ export default function Home() {
                         </div>
                         <Image
                             src={goomba}
-                            className="w-20 object-cover absolute bottom-24 right-[10rem] hover:animate-bounce nes-pointer"
+                            className="w-20 object-cover absolute bottom-24 right-[12rem] hover:animate-bounce nes-pointer"
                         />
                     </div>
                     <Image
@@ -206,18 +268,18 @@ export default function Home() {
                     <div class="brick w-full h-24 bg-white absolute bottom-0"></div>
                 </div>
             </section>
-			<div className="container mx-auto px-10 py-20 flex flex-col gap-20">
-				<div id="about" >
+			<div className="container mx-auto px-4 lg:px-10 py-10 flex flex-col gap-8 lg:gap-10">
+				<div id="about">
 					<div class="nes-container with-title">
 						<p class="title">ABOUT ME</p>
 						<h2 className="text-xl"><span className="nes-text is-primary text-xl me-3">#</span>I am a Front-End Developer</h2>
 						<p className="indent-10">committed to creating outstanding and valuable digital products. I possess a deep understanding of User Experience (UX) and User Interface (UI), coupled with strong Front-End coding skills. I enjoy tackling complex problems and am always looking for ways to improve and refine my work.</p>
 					</div>
 				</div>
-				<div className="grid grid-cols-2 gap-6">
+				<div className="grid lg:grid-cols-2 gap-8 lg:gap-10">
 					<div class="nes-container with-title">
 						<p class="title">PROFILE</p>
-						<div class="lists pl-10">
+						<div class="lists pl-3 lg:pl-10">
 							<ul class="nes-list is-circle grid gap-4">
 								<li>Name : Suniti Sukontaprapun</li>
 								<li>Nickname : Mon</li>
@@ -229,7 +291,7 @@ export default function Home() {
 					</div>
 					<div class="nes-container with-title">
 						<p class="title">EDUCATION</p>
-						<div class="lists pl-10">
+						<div class="lists pl-3 lg:pl-10">
 							<ul class="nes-list is-circle grid gap-4">
 								<li>University : Rajamangala University of Technology Suvarnabhumi</li>
 								<li>Degree : Bachelor</li>
@@ -241,9 +303,9 @@ export default function Home() {
 						</div>
 					</div>
 				</div>
-				<div class="nes-container with-title">
+				<div class="nes-container with-title border-y-0! border-x-0! pb-0! px-0! lg:border-y-[4px]! lg:border-x-[4px]! lg:px-[2rem]! lg:pb-[2rem]!">
 					<p class="title">WORK EXPERIENCE</p>
-					<div className="grid grid-cols-2 gap-6">
+					<div className="grid lg:grid-cols-2 gap-8 lg:gap-10">
 						<div class="nes-container with-title">
 							<p class="title">Emwork Group Co., Ltd.</p>
 							<div className="mb-4 pl-2">
@@ -254,7 +316,7 @@ export default function Home() {
 									Jun 2022 - Jan 2023
 								</h2>
 							</div>
-							<div class="lists pl-10">
+							<div class="lists pl-3 lg:pl-10">
 								<ul class="nes-list is-circle grid gap-4">
 									<li>Studied software testing at the unit, system, and integration levels.</li>
 									<li>Conducted user needs analysis.</li>
@@ -275,7 +337,7 @@ export default function Home() {
 									Feb 2023 - Sep 2024
 								</h2>
 							</div>
-							<div class="lists pl-10">
+							<div class="lists pl-3 lg:pl-10">
 								<ul class="nes-list is-circle grid gap-4">
 									<li>Gather user requirements.</li>
 									<li>Design and develop user interfaces.</li>
@@ -290,36 +352,122 @@ export default function Home() {
 				</div>
 				<div class="nes-container with-title">
 					<p class="title">MY TECH STACK</p>
-					<p className="indent-10 pb-10">I'm no pro, but I know these languages exeptionally well exept for React, which I am currently learning. I also have extensive experience in business management as well as team-work and leadership. My creativity is what puts me a cut above everyone else at my skill level, as well as how quickly I can learn new skills on the go!</p>
-
-					<div class="nes-container is-rounded grid gap-6">
-						<div className=" items-center justify-around gap-6 flex-wrap grid grid-cols-4">
+					<p className="indent-10 pb-4 lg:pb-10">I'm no pro, but I know these languages exeptionally well exept for React, which I am currently learning. I also have extensive experience in business management as well as team-work and leadership. My creativity is what puts me a cut above everyone else at my skill level, as well as how quickly I can learn new skills on the go!</p>
+					<div class="nes-container is-rounded grid gap-4 lg:gap-10">
+						<div  className="flex items-center gap-2 lg:gap-10">
+							<Image
+								src={progressImg}
+								className="w-12 h-12 object-contain"
+							/>
+							<progress class="nes-progress" value={progressValue} max="100"></progress>
+						</div>
+						<div className=" items-center justify-around gap-2 lg:gap-10 flex-wrap grid grid-cols-2 lg:grid-cols-4">
 							{logoLists.map((list, index) => 
-								<button type="button" class="nes-btn w-auto">
-									<div className="flex justify-start items-center gap-2">
-										<Image key={index} src={list.src} className="w-12 h-12 object-contain" />
-										{list.name}
+								<button type="button" class="nes-btn w-auto" onClick={() => handleBtnClick(list.score, list.src)}>
+									<div className="flex justify-center lg:justify-start items-center gap-2 text-sm">
+										<Image key={index} src={list.src} className="w-8 h-8 lg:w-12 lg:h-12 object-contain" />
+										<div className="hidden lg:block">
+											{list.name}
+										</div>
 									</div>
 								</button>
 							)}
 						</div>
-						<button type="button" class="nes-btn is-primary" onClick={toggleVisibility}>Primary</button>
-						{isVisible && (
-						<div className={`grid grid-cols-2 gap-6 `}>
-							{logoLists.map((list, index) => 
-							<div key={index} className="flex items-center gap-6">
-								<Image
-									src={list.src}
-									className="w-12 h-12 object-contain"
-								/>
-								<progress class="nes-progress" value={list.score} max="100"></progress>
-							</div>
-							)}
+					</div>
+				</div>
+				<div class="nes-container with-title px-4! lg:px-[2rem]!">
+					<p class="title">MY PROJECTS</p>
+					<div className="flex flex-col-reverse gap-6 lg:flex-col">
+						<div className="flex gap-4 justify-end">
+							<button type="button" class="nes-btn w-auto" onClick={scrollLeft}>
+								<Image src={arrow} className="w-10 object-contain rotate-180" />
+							</button>
+							<button type="button" class="nes-btn w-auto" onClick={scrollRight}>
+								<Image src={arrow} className="w-10 object-contain" />
+							</button>
 						</div>
-						)}
+						<div className="w-full flex overflow-x-scroll gap-6 snap-x snap-mandatory pb-0 lg:pb-6 box-project" ref={scrollableDivRef}>
+							{Array.from({ length: 6 }, (_, i) => (
+								<div key={i} class="nes-container with-title p-2! lg:p-[2rem]! is-centered lg:w-[26rem] grid gap-6 snap-center hover:scale-[98%]">
+									<Image src={moncookImg1} className="w-full h-[10rem] object-cover" />
+									<p className="text-xl text-start m-0!">Mon Cooking</p>
+									<div className="flex gap-3">
+										<button type="button" class="nes-btn is-disabled w-full">
+											Website
+										</button>
+										<button type="button" class="nes-btn w-full" onClick={openPopup}>
+											Detail
+										</button>
+									</div>
+								</div>
+							))}
+						</div>
+					</div>
+				</div>
+				<div class="nes-container with-title">
+					<p class="title">MY CONTSCT</p>
+					<div className="">
+						<div className="flex items-center gap-4 lg:gap-10 mb-6 flex-wrap">
+							<Link href="">
+								<i class="nes-icon facebook is-medium hover:-translate-y-4 transition-all duration-300 ease-in-out"></i>
+							</Link>
+							<Link href="">
+								<i class="nes-icon instagram is-medium hover:-translate-y-4 transition-all duration-300 ease-in-out"></i>
+							</Link>
+							<Link href="">
+								<i class="nes-icon github is-medium hover:-translate-y-4 transition-all duration-300 ease-in-out"></i>
+							</Link>
+							<Link href="">
+								<i class="nes-icon gmail is-medium hover:-translate-y-4 transition-all duration-300 ease-in-out"></i>
+							</Link>
+							<Link href="">
+								<i class="nes-icon linkedin is-medium hover:-translate-y-4 transition-all duration-300 ease-in-out"></i>
+							</Link>
+						</div>
+						<p className="indent-10">
+							If you are a potential client or employer then please feel free to contact me any time! Just make sure to include your name, your companies name, and what you're interested in. Thank you!!
+						</p>
 					</div>
 				</div>
 			</div>
+
+			{isPopupOpen && (
+				<section className="mx-auto px-4! lg:px-[2rem]! lg:w-1/2 fixed inset-x-0 top-8 lg:top-12 z-20">
+					<div class="nes-container with-title bg-white relative px-4! lg:px-[2rem]!">
+						<div class="nes-container with-title ">
+							<p class="title">text</p>
+							<div className="flex justify-center gap-3 mb-6">
+								<button type="button" class="nes-btn w-auto" onClick={scrollLeftPop}>
+									<Image src={arrow} className="w-10 object-contain rotate-180" />
+								</button>
+								<div className="w-full flex overflow-x-scroll gap-3 snap-x snap-mandatory box-project" ref={scrollableDivRefPop}>
+									{Array.from({ length: 6 }, (_, i) => (
+										<div key={i} class="w-[26rem] grid snap-center">
+											<div className="w-[16rem]">
+												<Image src={moncookImg1} className="w-full h-[10rem] object-cover" />
+											</div>
+										</div>
+									))}
+								</div>
+								<button type="button" class="nes-btn w-auto" onClick={scrollRightPop}>
+									<Image src={arrow} className="w-10 object-contain" />
+								</button>
+							</div>
+							<p className="indent-10 text-sm">committed to creating outstanding and valuable digital products. I possess a deep understanding of User Experience (UX) and User Interface (UI), coupled with strong Front-End coding skills. I e.</p>
+							
+							<div className="absolute -right-8 lg:-right-13 -top-13">
+								<button type="button" class="nes-btn is-error w-fit" onClick={closePopup}>
+									x
+								</button>
+							</div>
+						</div>
+					</div>
+				</section>
+			)}
+
+			{isPopupOpen && (
+				<div className="fixed top-0 left-0 right-0 bottom-0 w-full h-full opacity-50  bg-black z-10" onClick={closePopup}></div>
+			)}
         </div>
     );
 }
